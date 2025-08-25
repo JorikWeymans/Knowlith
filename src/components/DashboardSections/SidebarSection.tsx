45d49a397b3c6b3015848fd62e0d28a53bfba0bc
Jorik Weymans <jorikweymans@hotmail.be>
2025-08-25 15:57:52 +0200

    Finished sidebarSection visual, flipped logout

--- src/components/DashboardSections/SidebarSection.tsx
+++ src/components/DashboardSections/SidebarSection.tsx
@@ -7,20 +7,46 @@ import IconType from "@/types/IconType";
 
 const SidebarSection = () => {
     return (
-        <section className={"dashboard-section lsb"}>
-            <div className={"flex items-center p-large gap-4 border border-black"}>
+        <section className={"dashboard-section lsb flex flex-col p-medium"}>
+            <div className={"flex items-center p-large gap-4 "}>
                 <Image src={"/images/KnowlithLogo.png"} alt={"Knowlith Logo"} width={69} height={53} />
                 <p className={"font-bold text-secondary text-2xl tracking-[0.17em]"}>Knowlith</p>
             </div>
-            <div className={"border border-black mt-2 p-medium"}>
-                <Link href={"/"} className={"cursor-pointer flex items-center  bg-secondary p-large rounded-default gap-2"}>
-                    <Icon iconType={IconType.Home} colorClassName={"text-light-1"} ></Icon>
-
-                    <p className={"text-light-1 text-bold"}>Home</p>
-                </Link>
+            <div className={"mt-2 p-medium flex flex-col gap-2 grow"}>
+                <SideBarButton iconType={IconType.Home} text="Home" isHighlighted={true} />
+                <SideBarButton iconType={IconType.Courses_1} text="Courses" />
+                <SideBarButton iconType={IconType.Courses_2} text="All courses" />
+                <SideBarButton iconType={IconType.Home } text="All courses" />
             </div>
+            <div className={"mt-2 p-medium flex flex-col gap-2"}>
+                <SideBarButton iconType={IconType.Settings} text="Settings" />
+                <SideBarButton iconType={IconType.LogOut} text="Logout" textColorDarkClass={"text-error"} hasRedText={true}/>
+            </div>
+
+
         </section>
     );
 };
 
+interface SideBarButtonProps {
+    iconType: IconType ;
+    text: string;
+    isHighlighted?: boolean;
+    textColorLightClass?: string;
+    textColorDarkClass?: string;
+    hasRedText?: boolean;
+
+}
+const SideBarButton =  ({iconType, text, isHighlighted = false, hasRedText = false,} : SideBarButtonProps)=>
+{
+    const textColor = hasRedText? "text-error" : isHighlighted ? "text-light-1" : "text-dark-1";
+    const hoverText = hasRedText? "group-hover:text-light-1" : "group-hover:text-light-1";
+
+    return (
+        <Link href={"/"} className={`group cursor-pointer flex items-center p-large rounded-default gap-3 hover:bg-secondary hover:text-red-500
+                                     ${isHighlighted && "bg-secondary"}`}>
+            <Icon iconType={iconType} colorClassName={`${hoverText} ${textColor}`} ></Icon>
+            <p className={`font-semibold text-base ${hoverText} ${textColor}`}>{text}</p>
+        </Link>)
+}
 export default SidebarSection;
--- src/helpers/Icons.tsx
+++ src/helpers/Icons.tsx
@@ -56,7 +56,8 @@ const Icon = ({iconType, size = 24, colorClassName ="default"} : IconProps) => {
             return (
                 <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none"
                      stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                     className="feather feather-log-out">
+                     className="feather feather-log-out"
+                     transform="scale(-1 1)">
                     <path className={colorClassName!} d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
                     <polyline className={colorClassName!} points="16 17 21 12 16 7"></polyline>
                     <line className={colorClassName!} x1="21" y1="12" x2="9" y2="12"></line>
